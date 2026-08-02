7d313f66b4c251fdbec2ebfbbc533ac39b9f06c6
MOHAMMAD AMIR <simplifycodewithamir@gmail.com>
2026-08-02 10:14:06 +0530

    Add Entities folder and minor Program.cs tidy (#3)

--- source/HealthTracker.Server/Program.cs
+++ source/HealthTracker.Server/Program.cs
@@ -2,14 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
-builder.AddRedisClientBuilder("cache")
-    .WithOutputCache();
+builder.AddRedisClientBuilder("cache").WithOutputCache();
 
 // Add services to the container.
 builder.Services.AddProblemDetails();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
+
+//builder.Services
 
 var app = builder.Build();
 
